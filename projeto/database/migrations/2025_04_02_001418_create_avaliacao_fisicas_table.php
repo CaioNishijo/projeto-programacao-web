6f9ebdd70e4084723a9b5b3803b1acc08ca59b84
Caio Nishijo <cknishijo1@gmail.com>
2025-04-01 21:17:23 -0300

    criar avaliacao fisica

---
+++ projeto/database/migrations/2025_04_02_001418_create_avaliacao_fisicas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avaliacao_fisicas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->float('altura_cliente');
+            $table->float('peso_cliente');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('cliente')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avaliacao_fisicas');
+    }
+};
