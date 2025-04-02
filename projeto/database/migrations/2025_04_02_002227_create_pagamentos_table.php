1b939a6a40a9a687ce50a0591c4e4d975df262a2
Caio Nishijo <cknishijo1@gmail.com>
2025-04-01 21:23:59 -0300

    criar pagamento

---
+++ projeto/database/migrations/2025_04_02_002227_create_pagamentos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pagamentos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->float('valor');
+            $table->dateTime('data_pagamento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pagamentos');
+    }
+};
